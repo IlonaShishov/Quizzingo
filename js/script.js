d38a44ac6cd1bfec60a5e22499f4e1469b3a6806
Ilona Shishov <Ilona.Shishov@gmail.com>
2023-11-23 18:08:09 +0200

    chore: add simple timer
    Signed-off-by: Ilona Shishov <Ilona.Shishov@gmail.com>

--- js/script.js
+++ js/script.js
@@ -2,10 +2,20 @@ const gridContainer = document.querySelector(".grid-container");
 let cards = [];
 let greenScore = 0;
 let blueScore = 0;
+let blackScore = 0;
 let audioState = 0;
+
+const TIME_LIMIT = 10;
+let timePassed = 0;
+let timeLeft = TIME_LIMIT;
+let timerInterval = null;
+document.getElementById("timer").innerHTML = formatTime(
+  timeLeft
+);
 
 document.querySelector(".greenScore").textContent = greenScore;
 document.querySelector(".blueScore").textContent = blueScore;
+document.querySelector(".blackScore").textContent = blackScore;
 
 fetch("./data/cards.json")
   .then((res) => res.json())
@@ -54,6 +64,7 @@ function updateScore(element) {
       frontElement.classList.remove("greenTeam");
       greenScore--;
       document.querySelector(".greenScore").textContent = greenScore;
+
       frontElement.classList.add("blueTeam");
       blueScore++;
       document.querySelector(".blueScore").textContent = blueScore;
@@ -61,6 +72,15 @@ function updateScore(element) {
       frontElement.classList.remove("blueTeam");
       blueScore--;
       document.querySelector(".blueScore").textContent = blueScore;
+
+      frontElement.classList.add("blackTeam");
+      blackScore++;
+      document.querySelector(".blackScore").textContent = blackScore;
+    } else if (frontElement.classList.contains("blackTeam")) {
+      frontElement.classList.remove("blackTeam");
+      blackScore--;
+      document.querySelector(".blackScore").textContent = blackScore;
+
       frontElement.classList.add("greenTeam");
       greenScore++;
       document.querySelector(".greenScore").textContent = greenScore;
@@ -75,21 +95,70 @@ function updateScore(element) {
 function restart() {
   greenScore = 0;
   blueScore = 0;
+  blackScore = 0;
   document.querySelector(".greenScore").textContent = greenScore;
   document.querySelector(".blueScore").textContent = blueScore;
+  document.querySelector(".blackScore").textContent = blackScore;
   gridContainer.innerHTML = "";
   generateCards();
+  onTimesUp();
+  if(audioState != 0) {
+    playPause();
+  }
 }
 
 function playPause() {
   if(audioState == 0) {
+    startTimer();
     audioState = 1;
     document.getElementById('audio').play();
     document.getElementById('audio').volume = 0.3;
     document.getElementById('playPauseBtn').innerHTML = "Pause &#9208;";
   } else {
+    onTimesUp();
     audioState = 0;
     document.getElementById('audio').pause();
+    document.getElementById('audio').currentTime = 0;
     document.getElementById('playPauseBtn').innerHTML = "Play &#9658;";
   }
+}
+
+function onTimesUp() {
+  clearInterval(timerInterval);
+  restartTime();
+}
+
+function restartTime() {
+  timePassed = 0;
+  timeLeft = TIME_LIMIT;
+  timerInterval = null;
+  document.getElementById("timer").innerHTML = formatTime(
+    timeLeft
+  );
+}
+
+function startTimer() {
+  restartTime();
+  timerInterval = setInterval(() => {
+    timePassed = timePassed += 1;
+    timeLeft = TIME_LIMIT - timePassed;
+    document.getElementById("timer").innerHTML = formatTime(
+      timeLeft
+    );
+
+    if (timeLeft === 0) {
+      playPause();
+    }
+  }, 1000);
+}
+
+function formatTime(time) {
+  const minutes = Math.floor(time / 60);
+  let seconds = time % 60;
+
+  if (seconds < 10) {
+    seconds = `0${seconds}`;
+  }
+
+  return `${minutes}:${seconds}`;
 }
